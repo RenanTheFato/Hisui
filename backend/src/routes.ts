4fb5258ac3f5a65086c73c830c7c0c15389fd879
RenanTheFato <renan.santana007@hotmail.com>
2025-09-11 20:58:26 -0300

    apply schemas on routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,9 @@ import { VerifyEmailController } from "./controllers/users/verify-email-controll
 import { ResetPasswordController } from "./controllers/users/reset-password-controller.js";
 import { checkAdmin } from "./middlewares/admin-middleware.js";
 import { ListAllUsersController } from "./controllers/admin/list-all-users-controller.js";
+import { resetPasswordSchema } from "./swagger/schemas/users/reset-password-schema.js";
+import { verifyEmailSchema } from "./swagger/schemas/users/verify-email-schema.js";
+import { listAllUsersSchema } from "./swagger/schemas/admin/list-all-users-schema.js";
 
 export async function routes(fastify: FastifyTypedInstance) {
   fastify.get("/", async (req: FastifyRequest, rep: FastifyReply) => {
@@ -29,15 +32,15 @@ export async function routes(fastify: FastifyTypedInstance) {
     return new DeleteUserController().handle(req, rep)
   })
 
-  fastify.get("/verify-email", async (req: FastifyRequest, rep: FastifyReply) => {
+  fastify.get("/verify-email", { schema: verifyEmailSchema }, async (req: FastifyRequest, rep: FastifyReply) => {
     return new VerifyEmailController().handle(req, rep)
   })
 
-  fastify.patch("/reset-password", { preHandler: [authentication] }, async (req: FastifyRequest, rep: FastifyReply) => {
+  fastify.patch("/reset-password", { preHandler: [authentication], schema: resetPasswordSchema }, async (req: FastifyRequest, rep: FastifyReply) => {
     return new ResetPasswordController().handle(req, rep)
   })
 
-  fastify.get("/list-users", { preHandler: [authentication, checkAdmin] }, async (req: FastifyRequest, rep: FastifyReply) => {
+  fastify.get("/list-users", { preHandler: [authentication, checkAdmin], schema: listAllUsersSchema }, async (req: FastifyRequest, rep: FastifyReply) => {
     return new ListAllUsersController().handle(req, rep)
   })
 }
