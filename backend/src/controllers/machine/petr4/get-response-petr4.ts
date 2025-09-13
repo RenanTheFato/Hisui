d68800fa679a0ea9ce11482391e7e627cbe83702
RenanTheFato <renan.santana007@hotmail.com>
2025-09-13 17:02:36 -0300

    testing python server reesponse

--- backend/src/controllers/machine/petr4/get-response-petr4.ts
+++ backend/src/controllers/machine/petr4/get-response-petr4.ts
@@ -1,3 +1,10 @@
-export class GetResponsePetr4{
-  
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export class GetResponsePetr4 {
+  async handle(req: FastifyRequest, rep: FastifyReply) {
+    const response = await fetch(`${process.env.PYTHON_SERVER_URL}/health`)
+    const data = await response.json()
+
+    return rep.status(200).send({ message: data })
+  }
 }
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,10 +3,15 @@ import { userRoutes } from "./user-routes.js";
 import { FastifyTypedInstance } from "../@types/fastify-types.js";
 
 import { adminRoutes } from "./admin-routes.js";
+import { GetResponsePetr4 } from "../controllers/machine/petr4/get-response-petr4.js";
 
 export async function routes(fastify: FastifyTypedInstance) {
   fastify.get("/", async (req: FastifyRequest, rep: FastifyReply) => {
     return rep.status(200).send({ message: "Accepted" })
+  })
+
+  fastify.get("/machine", async (req: FastifyRequest, rep: FastifyReply) => {
+    return new GetResponsePetr4().handle(req, rep)
   })
 
   await userRoutes(fastify)
