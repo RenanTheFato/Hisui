145f76a70ac71082cd837c338dbad721c0fefa0e
RenanTheFato <renan.santana007@hotmail.com>
2025-10-25 18:29:50 -0300

    insert portfolio routes in to main routes file

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,9 +2,8 @@ import { FastifyTypedInstance } from "../@types/fastify-types.js";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { userRoutes } from "./user-routes.js";
 import { adminRoutes } from "./admin-routes.js";
-import { GetPythonServerStatusController } from "../controllers/machine/admin/get-python-server-status-controller.js";
-import { PredictPETR4Controller } from "../controllers/machine/petr4/predict-petr4-controller.js";
 import { machineRoutes } from "./machine-routes.js";
+import { portfolioRoutes } from "./portfolio-routes.js";
 
 export async function routes(fastify: FastifyTypedInstance) {
   fastify.get("/", async (req: FastifyRequest, rep: FastifyReply) => {
@@ -14,4 +13,5 @@ export async function routes(fastify: FastifyTypedInstance) {
   await userRoutes(fastify)
   await adminRoutes(fastify)
   await machineRoutes(fastify)
+  await portfolioRoutes(fastify)
 }
