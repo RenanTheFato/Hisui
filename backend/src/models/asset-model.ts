f2419b3b1b8a3d53d211aa56e6724d633ecec305
RenanTheFato <renan.santana007@hotmail.com>
2025-10-26 16:08:12 -0300

    increase assets data types

---
+++ backend/src/models/asset-model.ts
@@ -0,0 +1,12 @@
+export interface Assets {
+  id: string,
+  name: string,
+  ticker: string,
+  type: string,
+  sector: string,
+  company_name: string,
+  country: string,
+  exchange: string,
+  created_at: Date,
+  updated_at: Date,
+}
